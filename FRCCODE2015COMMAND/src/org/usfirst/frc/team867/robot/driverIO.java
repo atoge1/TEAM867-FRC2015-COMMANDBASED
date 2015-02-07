615965f6967972d2016d53562e11da85617dbb34
atoge1 <atoge888@gmail.com>
2015-02-07 09:19:47 -0800

    Added Preferences
    driver buttons and motor ports

--- FRCCODE2015COMMAND/src/org/usfirst/frc/team867/robot/driverIO.java
+++ FRCCODE2015COMMAND/src/org/usfirst/frc/team867/robot/driverIO.java
@@ -11,22 +11,22 @@ import org.usfirst.frc.team867.robot.commands.TeleDriveManager;
  * 
  * @author Andrew
  * @author Aaron
- * @version 2
+ * @version 3
  * 
  */
 public class driverIO 
 {
  
  Preferences prefs;
- Joystick joy = new Joystick(0);
- Button	drive = new JoystickButton(joy, 1);  // initiates safety button
- Button	reverse  = new JoystickButton(joy, 2); // initiates reverse button
- Button pivotccw  = new JoystickButton(joy, 4);// initiates counter-clockwise pivot button
- Button pivotcw  = new JoystickButton(joy, 5); // initiates clockwise button 
- double joyxaxis = joy.getRawAxis(0);
- double	joyyaxis = joy.getRawAxis(1);
- double joyzaxis = joy.getRawAxis(2);
- double driverjoyslow = joyzaxis; 
+ Joystick joy;
+ Button	drive;  // initiates safety button
+ Button	reverse; // initiates reverse button
+ Button pivotccw;// initiates counter-clockwise pivot button
+ Button pivotcw; // initiates clockwise button 
+ double joyxaxis;
+ double	joyyaxis;
+ double joyzaxis;
+ double driverjoyslow; 
  double frontLeftMotor;
  double rearLeftMotor;
  double frontRightMotor;
@@ -34,6 +34,18 @@ public class driverIO
  
  	public driverIO()
  	{ 	
+ 		//initialize 
+ 		 joy = new Joystick(prefs.getInt("driverJoyPort", 0));
+ 		 drive = new JoystickButton(joy, prefs.getInt("driveButton", 1));  // initiates safety button
+ 		 reverse  = new JoystickButton(joy, prefs.getInt("reverseButton", 2)); // initiates reverse button
+ 		 pivotccw  = new JoystickButton(joy, prefs.getInt("ccwButton", 4));// initiates counter-clockwise pivot button
+ 		 pivotcw  = new JoystickButton(joy, prefs.getInt("cwButton", 5)); // initiates clockwise button 
+ 		 joyxaxis = joy.getRawAxis(0);
+ 		 joyyaxis = joy.getRawAxis(1);
+ 		 joyzaxis = joy.getRawAxis(2);
+ 		
+ 		
+ 		
  		drive.whileHeld(new TeleDriveManager());
  		frontLeftMotor = prefs.getInt("frontLeftMotor", 2);
  		rearLeftMotor = prefs.getInt("rearLeftMotor", 1);
@@ -57,6 +69,15 @@ public class driverIO
  		return driverjoyslow;
  	}
  
+ 	public Button getCW()
+ 	{
+ 		return pivotcw;
+ 	}
+ 	
+ 	public Button getCCW()
+ 	{
+ 		return pivotccw;
+ 	}
  
  
  
